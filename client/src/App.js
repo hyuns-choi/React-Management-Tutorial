50757639c097b3ffbe111e2a30382320d7e70180
hyunseokchoi <newgensol@gmail.com>
2020-03-02 17:27:58 +0900

    Add Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -43,13 +44,35 @@ props or state => shouldComponentUpdate()
 
 class App extends Component {
 
-  state ={
-    customers:"",
-    completed: 0
+  constructor(props){
+    super(props);
+
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+
+    this.stateRefresh = this.stateRefresh.bind(this);
+ 
+  }
+
+  stateRefresh = () => {
+
+    console.log("---stateRefresh---")
+
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
   }
 
   // 마운트 완료 후
   componentDidMount(){
+    console.log("---componentDidMount---")
     this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers:res}))
@@ -57,6 +80,8 @@ class App extends Component {
   }
 
   callApi = async () => {
+    console.log("---callApi---")
+
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
@@ -70,42 +95,45 @@ class App extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table} >
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {
-            this.state.customers ? this.state.customers.map(c => {
-              return(
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            }) : 
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-              </TableCell>
-            </TableRow>
-          }
-          </TableBody>
-        </Table>        
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table} >
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {
+              this.state.customers ? this.state.customers.map(c => {
+                return(
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+            }
+            </TableBody>
+          </Table>        
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,38 +7,68 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect(function(error){
+
+	if(!!error){
+		console.log('error' + error.message);
+	}else{
+		console.log('OK!');
+	}
+});
+
+const multer = require('multer');
+const upload = multer({dest:'./upload'});
+
 app.get('/api/hello', (req, res) => {
     res.send({message:'Hello Express!!'});
 });
 
 app.get('/api/customers', (req,res) => {
-    res.send(
-        [{
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/any/1',
-            'name' : '이순신',
-            'birthday' : '901222',
-            'gender' : '여자',
-            'job' : '회사원'
-          },
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/any/2',
-            'name' : '홍길동',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/any/3',
-            'name' : '강남길',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-          }
-          
-        ]);
+    connection.query(
+      "select * from customer", 
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req,res) => {
+
+  console.log("---------start-------------");
+
+  let sql = 'INSERT INTO CUSTOMER VALUE (null, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+
+   //console.log(req.file);
+  // console.log(name);
+  // console.log(birthday);
+  // console.log(gender);
+   //console.log(job);
+
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+      //console.log(err);
+    }
+  );
+
 });
 
 app.listen(port, () => console.log(`Lisening on port ${port}`));
